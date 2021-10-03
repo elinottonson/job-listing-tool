637d2071af0455b9946e8d9c28256cab614b5f16
Alex Howe <howie242000@gmail.com>
2021-10-03 14:23:31 -0400

    omg we got it to work

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -4,32 +4,37 @@ const Login = () => {
     const [data, setData] = React.useState({ email: '', password: '' });
 
     const handleChange = (event) => {
-        setData({ email: event.target.value, password: event.target.value })
-    };
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(values => ({...values, [name]: value}));
+    }
 
     const handleSubmit = (event) => {
         const options = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json'},
-            body: body
+            body: JSON.stringify(data)
         };
+        console.log(`Sending request...`);
+        console.log(options.body);
         fetch("/api/login", options)
             .then((res) => res.json())
             .then((data) => {
                 setData(data.message)
                 console.log(data)
             });
+        event.preventDefault();
     };
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <label for="email">Email:</label><br/>
-                <input type="text" id="fname" name="email"/><br/>
+                <input type="text" onChange={handleChange} id="fname" name="email"/><br/>
                 <label for="password">Password:</label><br/>
-                <input type="password" id="lname" name="password"/>
+                <input type="password" onChange={handleChange} id="lname" name="password"/>
                 <input type='submit' value='Submit'/>
-            </form> 
+            </form>
         </div>
     );
 }
