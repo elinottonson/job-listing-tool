5fc9ac8bcdbb0f8f335ab7ea7a66e333f0fd4a19
MiniShark27 <kobifalus@gmail.com>
2021-10-08 01:20:39 -0400

    Added DB models to Backend

---
+++ Backend/sequelizeSetup/Models/Position.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+
+const PostionSchema = {
+        title : {
+            type: DataTypes.STRING,
+            },
+        companyName:{
+            type: DataTypes.STRING,
+        },
+        description : {
+            type: DataTypes.STRING,
+        },
+        minYearsExperience : {
+            type: DataTypes.INTEGER,
+            },
+        salary : {
+            type: DataTypes.INTEGER,
+            },
+        tags : {
+            type: DataTypes.ARRAY(DataTypes.STRING),
+            },
+        }
+
+module.exports =   ['Position',PostionSchema]
