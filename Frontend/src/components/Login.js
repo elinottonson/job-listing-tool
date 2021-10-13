bc41e927fec0c3dc99ffe5854ac10e3cb34d75c9
Alex Howe <howie242000@gmail.com>
2021-10-13 10:58:38 -0400

    New branch for demo

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -40,8 +40,7 @@ const Login = ({ setUser }) => {
     let validUser = false;
     let userKeys = Object.keys(userObj);
     
-    validUser = userKeys.includes('id') && userKeys.includes('employeeId') && userKeys.includes('companyId') &&
-                userKeys.includes('managerId') && userKeys.includes('isManager');
+    validUser = userKeys.includes('id') && userKeys.includes('employeeId') && userKeys.includes('companyId') && userKeys.includes('managerId') && userKeys.includes('isManager');
 
     // set user if valid
     if(validUser) {
