0be726454bcf99615d9113c266934c09480b6095
Alex Howe <howie242000@gmail.com>
2021-10-15 17:06:04 -0400

    Started work on listing components

---
+++ Frontend/src/components/JobListing.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+/*
+  Sample Job Listing Object
+  {
+    "id": 51,
+    "title": "Software Engineer I",
+    "companyName": "Techgenix",
+    "description": "Entry level Software Engineering role on an Agile team",
+    "minYearsExperience": 0,
+    "salary": 67848,
+    "tags": [
+      "Kotlin", 
+      "React",
+      "MySQL",
+      "Git"
+    ],
+    "createdAt": "2021-10-08T02:23:26.911Z",
+    "updatedAt": "2021-10-08T02:23:26.911Z"
+  }
+*/
+
+const JobListing = ({ listingObj }) => {
+  return (
+      <li className='job-listing'>
+        <div className='listing-header'>
+          <h2 id='title'>{listingObj.title}</h2>
+          <p id='salary'>{`$${listingObj.salary}`}</p>
+        </div>
+        <p className='listing-desc'>{listingObj.description}</p>
+        <div className='listing-footer'>
+            <ul className='tags'>
+              {listingObj.tags.map(tag => <li>{tag}</li>)}
+            </ul>
+            <p>{listingObj.createdAt}</p>
+        </div>
+      </li>
+  );
+};
+
+export default JobListing;
--- Frontend/src/components/JobListings.js
+++ Frontend/src/components/JobListings.js
@@ -1,10 +1,46 @@
-// TODO: Implement Job Listings
+import React from "react";
+import './../styles/Listings.css';
+
+import JobListing from "./JobListing";
+
 const JobListings = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+
+  const [ listings, setListings ] = React.useState([]);
+
+  React.useEffect(() => {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ company: 'Techgenix' })
+    };
+    
+    console.log('Sending job listings request...');
+
+    fetch('/api/listings', options)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log('Received response:')
+        console.log(data);
+        setListings(data);
+      })
+      .catch(e => { throw e; });
+  }, []);
+
+  return (
+    <div className='job-listings-container'>
+      <div className='listings-filter'>
+        <p>Filter</p>
+      </div>
+      <ul>
+        {listings.map(listing => <JobListing listingObj={listing} />)}
+      </ul>
+    </div>
+  );
+};
 
 export default JobListings;
