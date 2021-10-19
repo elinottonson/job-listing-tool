028beb9aa631452c8acf2c04b6b7f0b2480bf8f5
Martin Ma <Martin Ma@DESKTOP>
2021-10-18 21:18:08 -0400

    10/18/2021 Implemented a basic placeholder navbar for now. Edited DashboardNav.js and created Navbar.css in styles to style DashboardNav component.

--- Frontend/src/components/DashboardNav.js
+++ Frontend/src/components/DashboardNav.js
@@ -1,10 +1,20 @@
-// TODO: Implement Dashboard Nav
+import React from 'react';
+import './../styles/Navbar.css'
+
 const DashboardNav = () => {
     return (
-        <div>
-            <h2>NAV/SEARCH</h2>
-        </div>
-    )
+        <nav class='navbar'>
+            <div class='nav-left'>
+                <a>Home</a>
+                <a>Referrals</a>
+            </div>
+            <div class='nav-right'>
+                <a>Help</a>
+                <a>Notifications</a>
+                <a>User</a>
+            </div>
+        </nav>
+    );
 }
 
 export default DashboardNav;
