8f229ae31a5527e5e0417498eda12d1240376823
udayp1210 <uppatel@umass.edu>
2021-10-22 15:48:00 -0400

    started connecting frontend and backend with passport

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,49 @@
 const express = require('express');
 const app = express();
 
+const flash = require('express-flash'); 
+const passport = require('passport');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Initializes Passport
+const initializePassport = require('./passport/passport-config');
+const getUserById = require('./databaseInteraction/getUserById');
+initializePassport(
+  passport,
+  getUserById
+); 
+
 //For if a port is supplied as an enviroment variable
 const port = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 app.use(express.json());
-
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
-});
+app.use(flash());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'));
 
 const endpoints = [
   require('./endpoints/testGet'),
-  require('./endpoints/loginPost'),
+  require('./endpoints/testDash'),
+  require('./endpoints/logout'),
   require('./endpoints/listingsPost')
 ];
 
 for(const endpoint of endpoints){
   endpoint(app);
 }
+
+require('./endpoints/loginPost')(app, passport);
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
