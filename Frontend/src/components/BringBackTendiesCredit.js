33854caa57cf118c807a603d5b8aa29acc67f6a1
Alex Howe <howie242000@gmail.com>
2021-10-24 19:04:41 -0400

    Fixed some code formatting, a little bit of CSS spacing, and got the background image to take up the whole page

--- Frontend/src/components/BringBackTendiesCredit.js
+++ Frontend/src/components/BringBackTendiesCredit.js
@@ -2,11 +2,11 @@ import './../styles/Login.css';
 import logo from './../images/bringbacktendies.png';
 
 const BringBackTendiesCredit = () => {
-    return (
-        <div className='bringBackTendiesCredit'>
-            <img className='bbtLogo' src={logo} alt='Bring Back Tendies Logo'/>
-        </div>
-    )
+  return (
+    <div className='bringBackTendiesCredit'>
+      <img className='bbtLogo' src={logo} alt='Bring Back Tendies Logo'/>
+    </div>
+  )
 }
 
 export default BringBackTendiesCredit;
