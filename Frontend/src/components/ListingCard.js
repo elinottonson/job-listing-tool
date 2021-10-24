be666798c976b5512645ca7bf8fc5e906f56b7e4
Alex Howe <howie242000@gmail.com>
2021-10-24 17:52:28 -0400

    Initial popup done. Not sure about button placement and stuff, but it's definitely good enough

--- Frontend/src/components/ListingCard.js
+++ Frontend/src/components/ListingCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useParams, useHistory } from 'react-router-dom';
+import { FaTimesCircle } from 'react-icons/fa';
 import './../styles/ListingCard.css';
+import './../styles/Listings.css';
 
 const ListingCard = ({ setPopupOpen }) => {
 
@@ -14,6 +16,10 @@ const ListingCard = ({ setPopupOpen }) => {
       history.goBack();
     }
   };
+  const handleClose = () => {
+    setPopupOpen(false);
+    history.goBack();
+  };
 
   return (
     <div 
@@ -25,9 +31,30 @@ const ListingCard = ({ setPopupOpen }) => {
         onMouseEnter={()=> setHover(true)}
         onMouseLeave={() => setHover(false)}
       >
-        <p>LISTING CARD</p>
-        <p>ID: {id}</p>
-        <p>Hovering: {hover.toString()}</p>
+        <div className='listing-header'>
+          <div className='title-sal'>
+            <h2 id='title'>TITLE</h2>
+            <div id='subtitle'>
+              <p id='salary'>$SALARY</p>
+              <p id='st-break'>•</p>
+              <p id='popup-exp-level'>REQUIRED_EXPERIENCE</p>
+            </div>
+          </div>
+          <div className='popup-mng-dep'>
+            <p id='popup-mng-name'>MANAGER_NAME</p>
+            <p id='popup-mng-pos'>MANAGER_POS</p>
+          </div>
+          <FaTimesCircle 
+            id='close-btn'
+            onClick={handleClose}
+          />
+        </div>
+        <p className='popup-desc'>LISTING_DESCRIPTION</p>
+        <div className='listing-footer'>
+          <button type='button' id='ref-btn'>Leave Referral</button>
+          <ul className='popup-tags'>TAGS</ul>
+          <p id='popup-date'>DATE_CREATED</p>
+        </div>
       </div>
     </div>
   );
