f564f7befb2865df8135f85b9ecd942b4de9ead6
thomas-hoyt <thoyt@umass.edu>
2021-10-25 17:57:48 -0400

    Merge branch 'post-referrals' of https://github.com/elinottonson/job-listing-tool into post-referrals

--- Backend/endpoints/referralPost.js
+++ Backend/endpoints/referralPost.js
@@ -2,6 +2,40 @@ const {Express} = require('express');
 const postReferrals = require('../databaseInteraction/postReferrals');
 
 /**
+ * Determines if the supplied email is valid
+ * 
+ * @param {string} email the email being checked
+ * @returns {boolean} true if the email is valid, false otherwise
+ */
+function emailIsValid (email) {
+    return /\S+@\S+\.\S+/.test(email);
+}
+
+/**
+* Determines if string contains numbers.
+* 
+* @param {String} _string the string variable for first and last names
+* @returns {boolean} true if no numbers, false otherwise
+*/
+function nameHasNumbers(_string) {
+    return !(/\d/.test(_string));
+}
+
+/**
+* Checks if referral is valid.
+* 
+* @param {object} req the request send to backend.
+* @returns {boolean} true is referral is valid, false otherwise
+*/
+function isReferralValid (req) {
+    var refValid = false;
+    if (req.body.firstName != null && req.body.lastName != null && req.body.email != null && req.body.listingId != null) {
+        refValid = emailIsValid(req.body.email) && nameHasNumbers(req.body.firstName) && nameHasNumbers(req.body.lastName);
+    }
+    return refValid;
+}
+
+/**
  * Sets up new referral endpoint
  * 
  * @param {Express} app The express instance to setup the endpoint on
@@ -9,16 +43,22 @@ const postReferrals = require('../databaseInteraction/postReferrals');
  */
 function referralPost(app) {
     app.post('/api/new-referral', async(req, res) => {
-        const referral = {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            referralText: req.body.referralText,
-            listingId: req.body.listingId,
-            companyName: req.body.companyName,
-            authorId: req.body.authorId
+        if (isReferralValid(req)){
+            const referral = {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                referralText: req.body.referralText,
+                listingId: req.body.listingId,
+                companyName: req.body.companyName,
+                authorId: req.body.authorId
+            }
+            res.status(200);
+            res.send(await postReferrals(referral));
+        } else {
+            res.status(400);
+            res.send({ Error: 'Invalid referral.' });
         }
-        res.send(await postReferrals(referral));
     });
 }
 
