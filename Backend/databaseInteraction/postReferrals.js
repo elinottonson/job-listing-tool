2e2ac31de90d36358e7023defd6ed3064d3ee2c3
thomas-hoyt <91144886+thomas-hoyt@users.noreply.github.com>
2021-10-25 12:00:29 -0400

    Update postReferrals.js

--- Backend/databaseInteraction/postReferrals.js
+++ Backend/databaseInteraction/postReferrals.js
@@ -4,7 +4,7 @@ const {models} = require('../sequelizeSetup/sequalizeConstructor');
  * Pass new referrals to a listing
  *
  * @param {object} referral The object of the referral you want to create.
- * @returns {Promise<Array>} a promise evaluating to an array containing the referral objects affected
+ * @returns {Promise<models.Referral>} An instance of the Referral model
  * 
  */
 
