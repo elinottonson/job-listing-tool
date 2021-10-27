4763941a854a6352cb0bdb27e8c49a1f0b13f7f2
Spirolife <alenna.spiro@gmail.com>
2021-10-27 16:53:20 -0400

    Began creating filters section

--- Frontend/src/components/Dashboard.js
+++ Frontend/src/components/Dashboard.js
@@ -4,15 +4,23 @@ import Header from './Header';
 import DashboardNav from './DashboardNav';
 import JobListings from './JobListings';
 import Footer from './Footer';
+import Filters from './Filters.js';
 
 const Dashboard = ({ user }) => {
     return (
+    <div>
         <div className='dashboard'>
             <Header />
             <DashboardNav />
-            <JobListings user={user}/>
+            <div className='filters'>
+              <Filters/>
+            </div>
+            <div className='jobListings'>
+                <JobListings user={user}/>
+            </div>
             <Footer />
         </div>
+    </div>
     )
 }
 
---
+++ Frontend/src/components/Filters.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './../styles/Filters.css';
+
+const Filters = () => {
+    return (
+    <div className='filter'>
+        <h3>Tags</h3>
+        {/* <input type='checkbox' id='checkbox1'/>
+        <label>Java</label> */}
+        <h3>Experience Level</h3>
+        <h3>Department</h3>
+        <h3>Salary Range</h3>
+    </div>
+    );
+};
+
+export default Filters;
