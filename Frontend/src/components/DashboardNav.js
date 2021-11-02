98626056e96ad522794f5c756417620e7a4b2be0
Martin Ma <Martin Ma@DESKTOP>
2021-11-01 23:28:19 -0400

    Created function to handle what happens when the dashboardNav form is submitted. Currently all it does it pull text from the searchbar and store it in a local variable.

--- Frontend/src/components/DashboardNav.js
+++ Frontend/src/components/DashboardNav.js
@@ -5,12 +5,19 @@ import { FaSearch } from 'react-icons/fa'
 const DashboardNav = () => {
   return (
     <div className='dashboard-nav'>
-      <form className='dashboard-search'>
+      <form className='dashboard-search' onSubmit={handleSearchSubmit}>
         <input type='text' placeholder='Search...' className='search-box'></input>
         <button type='submit'><FaSearch id='search-icon'/></button>
       </form>
     </div>
   );
+};
+
+//searches based on values submitted from DashboardNav form
+const handleSearchSubmit = (event) =>{
+  event.preventDefault();
+  //get text from the searchbox
+  let searchInput = event.target[0].value;
 };
 
 export default DashboardNav;
