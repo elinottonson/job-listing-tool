8e173f0cc477c9e46ba9563d5e186d52bdf27153
Spirolife <alenna.spiro@gmail.com>
2021-11-11 15:12:12 -0500

    merge conflict

--- Frontend/src/components/Filters.js
+++ Frontend/src/components/Filters.js
@@ -7,6 +7,37 @@ const Slider = require('rc-slider');
 const createSliderWithTooltip = Slider.createSliderWithTooltip;
 const Range = createSliderWithTooltip(Slider.Range);
 
+const handleXp = props => {
+    const { value, dragging, index, ...restProps } = props;
+    return (
+      <SliderTooltip
+        prefixCls="rc-slider-tooltip"
+        overlay={`${value} Years`}
+        visible={dragging}
+        placement="top"
+        key={index}
+      >
+        <Handle value={value} {...restProps} />
+      </SliderTooltip>
+    );
+  };
+
+
+  const handleSal = props => {
+    const { value, dragging, index, ...restProps } = props;
+    return (
+      <SliderTooltip
+        prefixCls="rc-slider-tooltip"
+        overlay={`$${value}K`}
+        visible={dragging}
+        placement="top"
+        key={index}
+      >
+        <Handle value={value} {...restProps} />
+      </SliderTooltip>
+    );
+  };
+
 const Filters = ({ setFilterObj, filterObj }) => {
 
     const [tags, setTags] = React.useState([])
@@ -22,35 +53,26 @@ const Filters = ({ setFilterObj, filterObj }) => {
     }
 
     return (
-        <div className='filter' onClick={()=>setFilter(null,null,null,null,['SQL'])}>
-            <div class="filter-container">
-                <h3>Tags</h3>
-                <div class="form">
-                    <label>
-                        <input type="checkbox" onAuxClick={()=>setFilter(null,null,100000,null,null)}/>Java
-                    </label>
-                    <label>
-                        <input type="checkbox" />Javascript
-                    </label>
-                    <label>
-                        <input type="checkbox" />NodeJS
-                    </label>
-                    {tags.map(tag =>
-                        <label>
-                            <input type="checkbox" />{tag}
-                        </label>)}
-                </div>
+    <div className='filter'>
+        <div class="filter-container">
+            <h3>Tags</h3>
+            <div class="form">
+                {tags.map(tag => 
+                <label>
+                    <input type="checkbox"/>{tag}
+                </label>)}
             </div>
-            <div class="filter-container-ranges">
-                <h3>Experience Level</h3>
-                <div class="form">
-                    <Range allowCross={false} min={0} max={30} defaultValue={[0, 30]} ariaValueTextFormatterGroupForHandles={[(value) => "$" + value + " per year"]} />
-                </div>
-                <h3>Salary Range</h3>
-                <div class="form">
-                    <Range allowCross={false} min={0} max={100000} defaultValue={[0, 100000]} step={1000} />
-                </div>
+        </div>
+        <div class="filter-container-ranges">
+            <h3>Experience Level</h3>
+            <div class="form">
+                <Range allowCross={false} min={0} max={30} defaultValue={[0, 30]} handle={handleXp}/>
             </div>
+            <h3>Salary Range</h3>
+            <div class="form">
+                <Range allowCross={false} min={0} max={100000} defaultValue={[0, 100000]} step={10000} handle={handleSal}/>
+            </div>
+        </div>
         </div>
     );
 };
