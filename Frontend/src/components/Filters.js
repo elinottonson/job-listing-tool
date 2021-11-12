30dfe69f7c1981a123b9d3fefd8f82cee3c49394
Spirolife <alenna.spiro@gmail.com>
2021-11-12 15:09:27 -0500

    Implemented Select dropdown for tags

--- Frontend/src/components/Filters.js
+++ Frontend/src/components/Filters.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './../styles/Filters.css';
 import 'rc-slider';
 import 'rc-slider/assets/index.css';
+import Select from 'react-select'
 
 const Slider = require('rc-slider');
 const createSliderWithTooltip = Slider.createSliderWithTooltip;
@@ -23,54 +24,71 @@ const handleXp = props => {
   };
 
 
-  const handleSal = props => {
-    const { value, dragging, index, ...restProps } = props;
-    return (
-      <SliderTooltip
-        prefixCls="rc-slider-tooltip"
-        overlay={`$${value}K`}
-        visible={dragging}
-        placement="top"
-        key={index}
-      >
-        <Handle value={value} {...restProps} />
-      </SliderTooltip>
-    );
-  };
+  
+  const Filters = ({ setFilterObj, filterObj }) => {
+      
+    const handleSal = props => {
+      const { value, dragging, index, ...restProps } = props;
+      return (
+        <SliderTooltip
+          prefixCls="rc-slider-tooltip"
+          overlay={`$${value}K`}
+          visible={dragging}
+          placement="top"
+          key={index}
+        >
+          <Handle value={value} {...restProps} />
+        </SliderTooltip>
+      );
+    };
 
-const Filters = ({ setFilterObj, filterObj }) => {
+    const [tags, setTags] = React.useState(["SQL", "MySQL", "Angular", "NodeJS", "JavaScript", "Spark", "MongoDB"])
 
-    const [tags, setTags] = React.useState([])
-
-    function setFilter(minExpreience, maxExpreience, minSalery, maxSalery, tags) {
+    function setFilter(minExperience, maxExperience, minSalary, maxSalary, tags) {
         const obj = {};
-        obj.minExpreience = minExpreience ?? filterObj.minExpreience;
-        obj.minExpreience = maxExpreience ?? filterObj.maxExpreience;
-        obj.minSalery = minSalery ?? filterObj.minSalery;
-        obj.maxSalery = maxSalery ?? filterObj.maxSalery;
+        obj.minExperience = minExperience ?? filterObj.minExperience;
+        obj.minExperience = maxExperience ?? filterObj.maxExperience;
+        obj.minSalary = minSalary ?? filterObj.minSalary;
+        obj.maxSalary = maxSalary ?? filterObj.maxSalary;
         obj.tags = tags ?? filterObj.tags;
+        console.log(obj);
         setFilterObj(obj);
     }
 
     return (
     <div className='filter'>
-        <div class="filter-container">
+        <div class="tags-container">
             <h3>Tags</h3>
             <div class="form">
-                {tags.map(tag => 
-                <label>
-                    <input type="checkbox"/>{tag}
-                </label>)}
+                <Select className='selectTag' 
+                    isMulti={true} 
+                    options={tags.map(tag => {return {value: tag, label: tag}})}
+                    maxMenuHeight={200}
+                    // onChange={() => setFilter(0, 30, 0, 100, )}
+                />
             </div>
         </div>
         <div class="filter-container-ranges">
             <h3>Experience Level</h3>
             <div class="form">
-                <Range allowCross={false} min={0} max={30} defaultValue={[0, 30]} handle={handleXp}/>
+                <Range 
+                    allowCross={false} 
+                    min={0} 
+                    max={30} 
+                    defaultValue={[0, 30]} 
+                    handle={handleXp}
+                />
             </div>
             <h3>Salary Range</h3>
             <div class="form">
-                <Range allowCross={false} min={0} max={100000} defaultValue={[0, 100000]} step={10000} handle={handleSal}/>
+                <Range 
+                    allowCross={false} 
+                    min={0} 
+                    max={100} 
+                    defaultValue={[0, 100]} 
+                    step={10} 
+                    handle={handleSal}
+                />
             </div>
         </div>
         </div>
