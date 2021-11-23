0dd0e6a76be336f54334c612a4458c16a6009a46
MiniShark27 <kobifalus@gmail.com>
2021-11-22 19:04:01 -0500

    Temp

---
+++ Backend/endpoints/swaggerGet.js
@@ -0,0 +1,15 @@
+const { Express } = require('express');
+const { swaggerDocument } = require('../public/swagger.json');
+const swaggerUi = require('swagger-ui-express');
+
+/**
+ * Adds an endpoint to view the swagger documentation
+ * 
+ * @param {Express} app The express instance to setup the endpoint on
+ * @returns {void} Sets up the endpoint
+ */
+function addSwagger(app) {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
+
+module.exports = addSwagger;
